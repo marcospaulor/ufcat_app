f094d2883eeb975747ffcc46776970b62c4a72e5
marcospaulor <dev.silva.marcos@gmail.com>
2022-12-06 20:52:31 -0300

    resolvendo problemas de merging

--- lib/src/view/components/card.dart
+++ lib/src/view/components/card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:ufcat_app/view/const.dart';
-import 'package:ufcat_app/view/tabScreen.dart';
+import 'package:ufcat_app/src/view/style/const.dart';
+import 'package:ufcat_app/src/view/tabScreen.dart';
 import 'package:page_transition/page_transition.dart';
 
 class CardButton extends StatelessWidget {
