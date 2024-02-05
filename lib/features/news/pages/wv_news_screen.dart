0286a1eaf2b2034300da973860afcf8cd7667ef5
marcospaulor <dev.silva.marcos@gmail.com>
2024-02-05 17:33:06 -0300

    Refactor NewsEventNotice and add NewsList and NewsWebView

---
+++ lib/features/news/pages/wv_news_screen.dart
@@ -0,0 +1,129 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ufcat_app/shared/app_bar.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+class NewsWebView extends StatefulWidget {
+  final String url;
+  late final String titleAppBar;
+
+  NewsWebView({super.key, required this.url, this.titleAppBar = 'Tipo'});
+
+  @override
+  _NewsWebViewState createState() => _NewsWebViewState();
+}
+
+class _NewsWebViewState extends State<NewsWebView> {
+  final _key = UniqueKey();
+  bool isLoading = true;
+  late final WebViewController _controller;
+
+  String handleTitle(String title) {
+    // Mapa para mapear títulos para categorias correspondentes
+    Map<String, String> titleMapping = {
+      "noticia": "Notícia",
+      "evento": "Evento",
+      "edital": "Edital",
+    };
+
+    // Obtém a categoria correspondente ou retorna uma string vazia se não encontrada
+    return titleMapping[title.toLowerCase()] ?? '';
+  }
+
+  Future<bool> _goBack(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return Future.value(false);
+    } else {
+      Navigator.of(context).pop();
+      return Future.value(true);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.titleAppBar = handleTitle(widget.titleAppBar);
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onProgress: (int progress) {
+          print('WebView is loading (progress : $progress%)');
+          print(widget.url);
+        },
+        onPageStarted: (finish) {
+          setState(() {
+            isLoading = true;
+          });
+        },
+        onPageFinished: (finish) async {
+          setState(() {
+            isLoading = false;
+          });
+          await controller.runJavaScript('''
+            var mainContent = document.querySelector('main');
+            if (mainContent) {
+              document.body.innerHTML = '';
+              document.body.appendChild(mainContent);
+            }
+          ''');
+        },
+      ))
+      ..loadRequest(Uri.parse(widget.url));
+
+    _controller = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        WillPopScope(
+          onWillPop: () => _goBack(context),
+          child: Scaffold(
+            appBar: MyAppBar(
+              icon: FontAwesomeIcons.arrowLeft,
+              title: widget.titleAppBar,
+            ),
+            body: WebViewWidget(
+              key: _key,
+              controller: _controller,
+            ),
+          ),
+        ),
+        isLoading
+            ? Center(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Platform.isAndroid
+                      ? const CircularProgressIndicator()
+                      : const CupertinoActivityIndicator(),
+                ),
+              )
+            : Stack(),
+      ],
+    );
+  }
+}
---
+++ lib/features/tabs/widgets/new_list.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:ufcat_app/features/tabs/widgets/new_card.dart';
+
+class NewsList extends StatefulWidget {
+  final Future<List<Map<String, dynamic>>> futureInfos;
+  final String category;
+  final Future<List<Map<String, dynamic>>> Function() readJson;
+
+  const NewsList({
+    Key? key,
+    required this.futureInfos,
+    required this.category,
+    required this.readJson,
+  }) : super(key: key);
+
+  @override
+  _NewsListState createState() => _NewsListState();
+}
+
+class _NewsListState extends State<NewsList> {
+  late Future<List<Map<String, dynamic>>> _futureInfos;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureInfos = widget.futureInfos;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: _futureInfos,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return NewsCard(
+                imageUrl: '',
+                title: '',
+                date: '',
+                link: '',
+                category: widget.category,
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(
+            child: Text('No data available.'),
+          );
+        } else {
+          final infos = snapshot.data!;
+          final filteredInfos =
+              infos.where((info) => info['type'] == widget.category).toList();
+
+          return RefreshIndicator(
+            onRefresh: () async {
+              await Future.delayed(const Duration(seconds: 1));
+              setState(() {
+                _futureInfos = widget.readJson();
+              });
+            },
+            child: ListView.builder(
+              itemCount: filteredInfos.length,
+              itemBuilder: (context, index) {
+                return NewsCard(
+                  imageUrl: filteredInfos[index]['image_url'].toString(),
+                  title: filteredInfos[index]['title'].toString(),
+                  date: filteredInfos[index]['date'].toString(),
+                  link: filteredInfos[index]['link'].toString(),
+                  category: widget.category,
+                );
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+}
