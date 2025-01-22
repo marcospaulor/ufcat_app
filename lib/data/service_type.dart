1b26f341b0c1354ab78ed6ab3e353f776b6b7df9
marcospaulor <dev.silva.marcos@gmail.com>
2025-01-22 10:31:06 -0300

    Add categories, places, and service types data lists

---
+++ lib/data/service_type.dart
@@ -0,0 +1,12 @@
+List<String> service = [
+  'Instalações Hidrossanitárias',
+  'Instalações Elétricas',
+  'Instalações e/ou Manutenção em Cabeamento de Redes',
+  'Câmeras de Segurança',
+  'Pequenos Serviços de Reparos em Marcenaria',
+  'Serralheria',
+  'Reparos',
+  'Jardinagem',
+  'Vidraçaria',
+  'Pintura',
+];
