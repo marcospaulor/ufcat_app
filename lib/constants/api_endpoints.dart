55ff811de14bbbe88ab3879ee2f9aa422cc070a9
marcospaulor <dev.silva.marcos@gmail.com>
2025-02-27 15:55:32 -0300

    Refactor API endpoint constants to include a getter for base URL and improve error handling for missing environment variable

--- lib/constants/api_endpoints.dart
+++ lib/constants/api_endpoints.dart
@@ -1,11 +1,17 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiEndpoints {
-  static final _baseUrl = dotenv.env['API_URL'];
-  static final String _createOrder = '$_baseUrl/workorders/create/';
-  static final String _getCategories = '$_baseUrl/categories/';
-  static final String _getDepts = '$_baseUrl/depts/';
+  static final String _baseUrl = dotenv.env['API_URL'] ?? (throw Exception('API_URL not found in .env'));
 
+  // Usar o getter baseUrl para obter o valor de _baseUrl
+  static final String _createOrder = '$baseUrl/workorders/create/';
+  static final String _getCategories = '$baseUrl/categories/';
+  static final String _getDepts = '$baseUrl/depts/';
+
+  // Getter para o _baseUrl
+  static String get baseUrl => _baseUrl;
+
+  // Getters para os endpoints
   static String get createOrder => _createOrder;
   static String get getCategories => _getCategories;
   static String get getDepts => _getDepts;
