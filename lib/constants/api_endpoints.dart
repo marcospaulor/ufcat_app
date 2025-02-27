ab0013ff328f2c11ba9d485c50cf65ab4aa8447c
marcospaulor <dev.silva.marcos@gmail.com>
2025-02-27 15:31:48 -0300

    Refactor API endpoint constants to use private fields and getters for improved encapsulation

--- lib/constants/api_endpoints.dart
+++ lib/constants/api_endpoints.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiEndpoints {
-  static final baseUrl = dotenv.env['API_URL'];
-  static final createOrder = '$baseUrl/workorders/create/';
-  static final getCategories = '$baseUrl/categories/';
-  static final String getDepts = '$baseUrl/depts/';
+  static final _baseUrl = dotenv.env['API_URL'];
+  static final String _createOrder = '$_baseUrl/workorders/create/';
+  static final String _getCategories = '$_baseUrl/categories/';
+  static final String _getDepts = '$_baseUrl/depts/';
+
+  static String get createOrder => _createOrder;
+  static String get getCategories => _getCategories;
+  static String get getDepts => _getDepts;
 }
