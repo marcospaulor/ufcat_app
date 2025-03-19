1cfed829434b172f6f63c680be2c8814a86135b4
marcospaulor <dev.silva.marcos@gmail.com>
2025-03-19 04:38:18 -0300

    Update app label in AndroidManifest.xml for consistency

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Serviços UFCAT',
+      title: 'UFCAT Serviços',
       theme: ThemeData(
         pageTransitionsTheme: const PageTransitionsTheme(
           builders: {
